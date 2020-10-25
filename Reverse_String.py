ff97cb21e537f3926f7bfc7054a0549b629803ef
suraj <surajsharma@pm.me>
2020-10-25 07:48:27 +0530

    Reverse String

---
+++ Reverse_String.py
@@ -0,0 +1,8 @@
+S='Reverse this string'
+
+def reverseString(S):
+
+    l = len(S)-1
+    while l > 0:
+        print(l)
+reverseString(S)
--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,22 +1,16 @@
 A = [23, 3, 45, 4, 56, 4, 5, 6, 5, 6, 7, 8]
 
-
 def sortAnArray(A):
     # Traverse through all array elements
     for i in range(len(A)):
-
         # Find the minimum element in remaining
-        # unsorted array
         min_idx = i
         for j in range(i+1, len(A)):
             if A[min_idx] > A[j]:
                 min_idx = j
-
         # Swap the found minimum element with
-        # the first element
         A[i], A[min_idx] = A[min_idx], A[i]
     return A
-
 
 sorted = sortAnArray(A)
 print(sorted)
