5e1d1c4e1a4fb12a7ff936a45af79a63b0569f36
suraj <surajsharma@pm.me>
2020-10-26 17:48:45 +0530

    Selection Sort Recursive

--- Selection_Sort_Recursive.py
+++ Selection_Sort_Recursive.py
@@ -1,8 +1,28 @@
-A = [23, 3, 45, 4, 56]
+A = [44, 33, 22, 11, 00]
 
-#23, 3, 45, 4, 56
-#23, 3, 45, 4, 56
-#23, 3, 4, 45, 56
-#23, 3, 4, 45, 56
-#3, 23, 4, 45, 56
-#3, 4, 23, 45, 56
+# def sortAnArray(A):
+#     # Traverse through all array elements
+#     for i in range(len(A)):
+#         # Find the minimum element in remaining
+#         min_idx = i
+#         for j in range(i+1, len(A)):
+#             if A[min_idx] > A[j]:
+#                 min_idx = j
+#         # Swap the found minimum element with
+#         A[i], A[min_idx] = A[min_idx], A[i]
+#     return A
+
+
+def select_sort_rec(a):
+    if len(a) <= 1:
+        return a
+    else:
+        b = list(a)
+        m = min(b)
+        b.remove(m)
+
+        return [m] + select_sort_rec(b)
+
+
+sorted = select_sort_rec(A)
+print(sorted)
